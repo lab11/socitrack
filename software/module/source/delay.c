bd9dfe1db7a965556edeb6855b8ae37739ae07a3
abiri <abiri@bluewin.ch>
2018-10-05 22:14:49 -0700

    corrected delay values, added delay values for new board

--- software/module/source/delay.c
+++ software/module/source/delay.c
@@ -1,4 +1,5 @@
 #include "delay.h"
+#include "board.h"
 #include "stm32f0xx.h"
 
 #define USECOND 1e9
@@ -6,10 +7,18 @@
 
 void uDelay(uint32_t u) {
 	volatile uint32_t i = 0;
-	for (i = 0; i < ((float)SystemCoreClock/(float)USECOND*13)*u; i++) { }
+#if BOARD_V == TRIPOINT
+	for (i = 0; i < ((float)SystemCoreClock/(float)USECOND * 12)*u; i++) { }
+#elif BOARD_V == SQUAREPOINT
+	for (i = 0; i < ((float)SystemCoreClock/(float)USECOND * 6)*u; i++) { }
+#endif
 }
 
 void mDelay(uint32_t m) {
 	volatile uint32_t i = 0;
-	for (i = 0; i < (SystemCoreClock/MSECOND*9)*m; i++) { }
+#if BOARD_V == TRIPOINT
+	for (i = 0; i < ((float)SystemCoreClock/(float)MSECOND * 12)*m; i++) { }
+#elif BOARD_V == SQUAREPOINT
+	for (i = 0; i < ((float)SystemCoreClock/(float)MSECOND * 6)*m; i++) { }
+#endif
 }
