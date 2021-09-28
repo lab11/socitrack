1f3649e00e9ea920281137f3421a859ffb2c7643
Will Hedgecock <will.hedgecock@gmail.com>
2021-09-28 13:47:22 -0500

    Migrate TWI to not use deprecated functions

--- software/tottag/firmware/peripherals/src/squarepoint_interface.c
+++ software/tottag/firmware/peripherals/src/squarepoint_interface.c
@@ -22,7 +22,13 @@ static squarepoint_interface_data_callback _data_callback = NULL;
 
 static const nrfx_twi_t _twi_instance = NRFX_TWI_INSTANCE(1);
 static bool _twi_initialized = false;
-static uint8_t _twi_rx_buf[256];
+static uint8_t _twi_rx_buf[256], _twi_rx_length;
+static const nrfx_twi_xfer_desc_t _rx_length_description = { .type = NRFX_TWI_XFER_RX, .address = SQUAREPOINT_ADDRESS,
+                                                             .p_primary_buf = &_twi_rx_length, .p_secondary_buf = NULL,
+                                                             .primary_length = 1, .secondary_length = 0 };
+static nrfx_twi_xfer_desc_t _rx_data_description = { .type = NRFX_TWI_XFER_RX, .address = SQUAREPOINT_ADDRESS,
+                                                     .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                                     .primary_length = 1, .secondary_length = 0 };
 
 
 // Private helper functions --------------------------------------------------------------------------------------------
@@ -72,19 +78,26 @@ static void squarepoint_interrupt_handler(nrfx_gpiote_pin_t pin, nrf_gpiote_pola
       return;
 
    // Read the length of the incoming packet and ensure that it is valid
-   uint8_t cmd = SQUAREPOINT_CMD_READ_PACKET_LENGTH;
-   if ((nrfx_twi_tx(&_twi_instance, SQUAREPOINT_ADDRESS, &cmd, sizeof(cmd), false) != NRFX_SUCCESS) ||
-         (nrfx_twi_rx(&_twi_instance, SQUAREPOINT_ADDRESS, _twi_rx_buf, 1) != NRFX_SUCCESS) ||
-         !_twi_rx_buf[0] || (_twi_rx_buf[0] == 0xFF))
+   _twi_rx_buf[0] = SQUAREPOINT_CMD_READ_PACKET_LENGTH;
+   nrfx_twi_xfer_desc_t tx_description = { .type = NRFX_TWI_XFER_TX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 1, .secondary_length = 0 };
+   if ((nrfx_twi_xfer(&_twi_instance, &tx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER) != NRFX_SUCCESS) ||
+       (nrfx_twi_xfer(&_twi_instance, &_rx_length_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER) != NRFX_SUCCESS) ||
+       !_twi_rx_length || (_twi_rx_length == 0xFF))
+   {
+      log_printf("ERROR: Failed reading SquarePoint packet length\n");
       return;
+   }
 
    // Read the rest of the incoming packet
-   cmd = SQUAREPOINT_CMD_READ_PACKET;
-   if ((nrfx_twi_tx(&_twi_instance, SQUAREPOINT_ADDRESS, &cmd, sizeof(cmd), false) == NRFX_SUCCESS) &&
-         (nrfx_twi_rx(&_twi_instance, SQUAREPOINT_ADDRESS, _twi_rx_buf + 1, _twi_rx_buf[0]) == NRFX_SUCCESS))
+   _twi_rx_buf[0] = SQUAREPOINT_CMD_READ_PACKET;
+   _rx_data_description.primary_length = _twi_rx_length;
+   if ((nrfx_twi_xfer(&_twi_instance, &tx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER) == NRFX_SUCCESS) &&
+       (nrfx_twi_xfer(&_twi_instance, &_rx_data_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER) == NRFX_SUCCESS))
       nrfx_atomic_flag_set(_squarepoint_interrupt_thrown);
    else
-      log_printf("ERROR: Failed reading SquarePoint packet of length %i\n", _twi_rx_buf[0]);
+      log_printf("ERROR: Failed reading SquarePoint packet of length %i\n", _twi_rx_length);
 }
 
 static nrfx_err_t twi_hw_init(void)
@@ -125,14 +138,22 @@ static void twi_hw_uninit(void)
 static nrfx_err_t squarepoint_get_info(uint16_t *id, uint8_t *version, const uint8_t *eui)
 {
    // Requesting a device info string from the SquarePoint module
-   uint8_t cmd[7] = { SQUAREPOINT_CMD_INFO, eui[0], eui[1], eui[2], eui[3], eui[4], eui[5] }, resp[3] = { 0 };
-   nrfx_err_t err_code = nrfx_twi_tx(&_twi_instance, SQUAREPOINT_ADDRESS, cmd, sizeof(cmd), false);
+   _twi_rx_buf[0] = SQUAREPOINT_CMD_INFO;
+   _twi_rx_buf[1] = eui[0]; _twi_rx_buf[2] = eui[1]; _twi_rx_buf[3] = eui[2];
+   _twi_rx_buf[4] = eui[3]; _twi_rx_buf[5] = eui[4]; _twi_rx_buf[6] = eui[5];
+   nrfx_twi_xfer_desc_t tx_description = { .type = NRFX_TWI_XFER_TX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 7, .secondary_length = 0 },
+                        rx_description = { .type = NRFX_TWI_XFER_RX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 3, .secondary_length = 0 };
+   nrfx_err_t err_code = nrfx_twi_xfer(&_twi_instance, &tx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER);
    if (err_code == NRFX_SUCCESS)
-      err_code = nrfx_twi_rx(&_twi_instance, SQUAREPOINT_ADDRESS, resp, sizeof(resp));
+      err_code = nrfx_twi_xfer(&_twi_instance, &rx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER);
    if (err_code == NRFX_SUCCESS)
    {
-      *id = (uint16_t)((uint16_t)resp[0] << (uint8_t)8) | resp[1];
-      *version = resp[2];
+      *id = (uint16_t)((uint16_t)_twi_rx_buf[0] << (uint8_t)8) | _twi_rx_buf[1];
+      *version = _twi_rx_buf[2];
    }
    else
       *id = *version = 0;
@@ -172,40 +193,65 @@ nrfx_err_t squarepoint_start_application(uint32_t current_time, uint8_t device_r
 {
    // Send an outgoing command requesting that the SquarePoint module start
    log_printf("INFO: Starting SquarePoint module as %s with timestamp %lu\n", (scheduler_role == SCHEDULER) ? "SCHEDULER" : "PARTICIPANT", current_time);
-   uint8_t cmd[6] = { SQUAREPOINT_CMD_START, device_role | (scheduler_role << 3) | (SQUAREPOINT_RUNTIME_MODE_STANDARD << 6), 0, 0, 0, 0 };
-   memcpy(cmd + 2, &current_time, sizeof(current_time));
-   return nrfx_twi_tx(&_twi_instance, SQUAREPOINT_ADDRESS, cmd, sizeof(cmd), false);
+   _twi_rx_buf[0] = SQUAREPOINT_CMD_START;
+   _twi_rx_buf[1] = device_role | (scheduler_role << 3) | (SQUAREPOINT_RUNTIME_MODE_STANDARD << 6);
+   memcpy(_twi_rx_buf + 2, &current_time, sizeof(current_time));
+   nrfx_twi_xfer_desc_t tx_description = { .type = NRFX_TWI_XFER_TX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 6, .secondary_length = 0 };
+   return nrfx_twi_xfer(&_twi_instance, &tx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER);
 }
 
 nrfx_err_t squarepoint_start_calibration(uint8_t index)
 {
    // Send an outgoing command requesting that the SquarePoint module enter calibration mode
-   uint8_t cmd[3] = { SQUAREPOINT_CMD_START, (SQUAREPOINT_RUNTIME_MODE_CALIBRATION << 6), index };
-   return nrfx_twi_tx(&_twi_instance, SQUAREPOINT_ADDRESS, cmd, sizeof(cmd), false);
+   _twi_rx_buf[0] = SQUAREPOINT_CMD_START;
+   _twi_rx_buf[1] = (SQUAREPOINT_RUNTIME_MODE_CALIBRATION << 6);
+   _twi_rx_buf[2] = index;
+   nrfx_twi_xfer_desc_t tx_description = { .type = NRFX_TWI_XFER_TX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 3, .secondary_length = 0 };
+   return nrfx_twi_xfer(&_twi_instance, &tx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER);
 }
 
 nrfx_err_t squarepoint_get_calibration(uint8_t *calib_buf)
 {
    // Send an outgoing command requesting the SquarePoint calibration info
-   uint8_t cmd = SQUAREPOINT_CMD_READ_CALIBRATION;
-   nrfx_err_t err_code = nrfx_twi_tx(&_twi_instance, SQUAREPOINT_ADDRESS, &cmd, sizeof(cmd), false);
+   _twi_rx_buf[0] = SQUAREPOINT_CMD_READ_CALIBRATION;
+   nrfx_twi_xfer_desc_t tx_description = { .type = NRFX_TWI_XFER_TX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 1, .secondary_length = 0 },
+                        rx_description = { .type = NRFX_TWI_XFER_RX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = calib_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 18, .secondary_length = 0 };
+   nrfx_err_t err_code = nrfx_twi_xfer(&_twi_instance, &tx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER);
    if (err_code == NRFX_SUCCESS)
-      err_code = nrfx_twi_rx(&_twi_instance, SQUAREPOINT_ADDRESS, calib_buf, 18);
+      err_code = nrfx_twi_xfer(&_twi_instance, &rx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER);
    return err_code;
 }
 
 nrfx_err_t squarepoint_stop(void)
 {
    // Send an outgoing command requesting that the SquarePoint module stop
-   uint8_t cmd = SQUAREPOINT_CMD_STOP;
-   return nrfx_twi_tx(&_twi_instance, SQUAREPOINT_ADDRESS, &cmd, sizeof(cmd), false);
+   _twi_rx_buf[0] = SQUAREPOINT_CMD_STOP;
+   nrfx_twi_xfer_desc_t tx_description = { .type = NRFX_TWI_XFER_TX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 1, .secondary_length = 0 };
+   return nrfx_twi_xfer(&_twi_instance, &tx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER);
 }
 
 nrfx_err_t squarepoint_set_time(uint32_t epoch)
 {
    // Send an outgoing command requesting that the SquarePoint module reset its current time epoch
-   uint8_t cmd[5] = { SQUAREPOINT_CMD_SET_TIME, (epoch >> 3 * 8) & 0xFF, (epoch >> 2 * 8) & 0xFF, (epoch >> 1 * 8) & 0xFF, (epoch >> 0 * 8) & 0xFF };
-   return nrfx_twi_tx(&_twi_instance, SQUAREPOINT_ADDRESS, cmd, sizeof(cmd), false);
+   _twi_rx_buf[0] = SQUAREPOINT_CMD_SET_TIME;
+   _twi_rx_buf[1] = (epoch >> 3 * 8) & 0xFF;
+   _twi_rx_buf[2] = (epoch >> 2 * 8) & 0xFF;
+   _twi_rx_buf[3] = (epoch >> 1 * 8) & 0xFF;
+   _twi_rx_buf[4] = (epoch >> 0 * 8) & 0xFF;
+   nrfx_twi_xfer_desc_t tx_description = { .type = NRFX_TWI_XFER_TX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 5, .secondary_length = 0 };
+   return nrfx_twi_xfer(&_twi_instance, &tx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER);
 }
 
 nrfx_err_t squarepoint_wakeup_module(void)
@@ -233,20 +279,26 @@ nrfx_err_t squarepoint_wakeup_module(void)
 nrfx_err_t squarepoint_wakeup_radio(void)
 {
    // Send an outgoing command requesting that the SquarePoint module's radio wakeup
-   uint8_t cmd = SQUAREPOINT_CMD_WAKEUP;
-   return nrfx_twi_tx(&_twi_instance, SQUAREPOINT_ADDRESS, &cmd, sizeof(cmd), false);
+   _twi_rx_buf[0] = SQUAREPOINT_CMD_WAKEUP;
+   nrfx_twi_xfer_desc_t tx_description = { .type = NRFX_TWI_XFER_TX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 1, .secondary_length = 0 };
+   return nrfx_twi_xfer(&_twi_instance, &tx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER);
 }
 
 nrfx_err_t squarepoint_ack(void)
 {
    // Send an outgoing command responding to a ping request
-   uint8_t cmd = SQUAREPOINT_CMD_ACK;
-   return nrfx_twi_tx(&_twi_instance, SQUAREPOINT_ADDRESS, &cmd, sizeof(cmd), false);
+   _twi_rx_buf[0] = SQUAREPOINT_CMD_ACK;
+   nrfx_twi_xfer_desc_t tx_description = { .type = NRFX_TWI_XFER_TX, .address = SQUAREPOINT_ADDRESS,
+                                           .p_primary_buf = _twi_rx_buf, .p_secondary_buf = NULL,
+                                           .primary_length = 1, .secondary_length = 0 };
+   return nrfx_twi_xfer(&_twi_instance, &tx_description, NRFX_TWI_FLAG_NO_XFER_EVT_HANDLER);
 }
 
 void squarepoint_handle_incoming_data(void)
 {
    // Call the user callback
    if (_data_callback)
-      _data_callback(_twi_rx_buf + 1, _twi_rx_buf[0]);
+      _data_callback(_twi_rx_buf, _twi_rx_length);
 }
