72bad8c50422eb3527da46f6e7022aed60c5319f
Will Hedgecock <will.hedgecock@gmail.com>
2022-04-26 15:43:35 -0500

    Simplify internal RTC functionality

--- software/tottag/firmware/peripherals/src/rtc.c
+++ software/tottag/firmware/peripherals/src/rtc.c
@@ -15,10 +15,7 @@
 // Static RTC state variables ------------------------------------------------------------------------------------------
 
 static const nrfx_rtc_t _rtc_instance = NRFX_RTC_INSTANCE(2);
-static nrfx_atomic_u32_t _rtc_sync_time = 0;
-static nrfx_atomic_u32_t _rtc_sync_rtc_counter = 0;
-static nrfx_atomic_u32_t _rtc_sync_rtc_overflow_counter = 0;
-static nrfx_atomic_flag_t _rtc_sync_rtc_overflown = false;
+static nrfx_atomic_u32_t _rtc_sync_time, _rtc_sync_rtc_counter;
 
 
 // Private helper functions --------------------------------------------------------------------------------------------
@@ -32,40 +29,35 @@ static uint32_t rtc_to_s(uint32_t ticks) { return ticks / 8; }
 void rtc_init(void)
 {
    // Initialize the RTC instance
+   uint32_t dummy_val;
    nrfx_rtc_config_t rtc_config = NRFX_RTC_DEFAULT_CONFIG;
    rtc_config.prescaler = 4095;  // Ticks at 8 Hz; PRESCALER is 12bit register (2^12 - 1 = 4095)
-   nrfx_err_t err_code = nrfx_rtc_init(&_rtc_instance, &rtc_config, rtc_handler);
-   APP_ERROR_CHECK(err_code);
+   _rtc_sync_time = _rtc_sync_rtc_counter = 0;
+   nrfx_rtc_counter_clear(&_rtc_instance);
+   APP_ERROR_CHECK(nrfx_rtc_init(&_rtc_instance, &rtc_config, rtc_handler));
+   nrfx_rtc_int_disable(&_rtc_instance, &dummy_val);
+   nrfx_rtc_overflow_disable(&_rtc_instance);
+   nrfx_rtc_tick_disable(&_rtc_instance);
    nrfx_rtc_enable(&_rtc_instance);
 }
 
 uint32_t rtc_get_current_time(void)
 {
-   // Get current application time
-   uint32_t current_rtc_counter = rtc_to_s(nrfx_rtc_counter_get(&_rtc_instance));
-   uint32_t time = _rtc_sync_time + (current_rtc_counter - _rtc_sync_rtc_counter);
-
    // Check for an RTC overflow
-   if (!_rtc_sync_rtc_overflown && (current_rtc_counter < _rtc_sync_rtc_counter))
+   uint32_t current_rtc_counter = nrfx_rtc_counter_get(&_rtc_instance);
+   if (current_rtc_counter < _rtc_sync_rtc_counter)
    {
-      log_printf("WARNING: RTC overflow has occurred\n");
-      _rtc_sync_rtc_overflown = true;
-      ++_rtc_sync_rtc_overflow_counter;
-   }
-   else if (_rtc_sync_rtc_overflown && (current_rtc_counter > _rtc_sync_rtc_counter))
-   {
-      log_printf("INFO: RTC overflow flag cleared, ready for next overflow\n");
-      _rtc_sync_rtc_overflown = false;
+      _rtc_sync_time += rtc_to_s(current_rtc_counter + 0x00FFFFFF - _rtc_sync_rtc_counter);
+      _rtc_sync_rtc_counter = current_rtc_counter;
    }
 
-   // Include RTC overflow and return time
-   return time + (_rtc_sync_rtc_overflow_counter * rtc_to_s(0x00FFFFFF));
+   // Return the current application time
+   return _rtc_sync_time + rtc_to_s(current_rtc_counter - _rtc_sync_rtc_counter);
 }
 
 void rtc_set_current_time(uint32_t epoch)
 {
+   // Set the current application time
    _rtc_sync_time = epoch;
-   _rtc_sync_rtc_counter = rtc_to_s(nrfx_rtc_counter_get(&_rtc_instance));
-   _rtc_sync_rtc_overflow_counter = 0;
-   _rtc_sync_rtc_overflown = false;
+   _rtc_sync_rtc_counter = nrfx_rtc_counter_get(&_rtc_instance);
 }
