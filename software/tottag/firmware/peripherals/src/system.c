2c8a2be34c92b4d6795db7b4ac3ff4995bc3d9f7
Will Hedgecock <will.hedgecock@gmail.com>
2022-07-20 13:42:56 -0500

    Allow unused pins to autoinitialize to "disconnnected input"

--- software/tottag/firmware/peripherals/src/system.c
+++ software/tottag/firmware/peripherals/src/system.c
@@ -1,20 +1,10 @@
 // Header inclusions ---------------------------------------------------------------------------------------------------
 
-#include "ble_config.h"
-#include "nrfx_gpiote.h"
 #include "system.h"
-
-typedef struct gpio_pin_def_t { uint8_t a, b; } gpio_pin_def_t;
 
 void initialize_gpio(void)
 {
    // Initialize the GPIO subsystem
    if (!nrfx_gpiote_is_init())
       nrfx_gpiote_init();
-
-   // Set all unused pins to INPUT ANALOG
-   gpio_pin_def_t unused_pins[] = UNUSED_GPIO_PINS;
-   uint32_t num_unused_pins = sizeof(unused_pins) / sizeof(gpio_pin_def_t);
-   for (uint32_t i = 0; i < num_unused_pins; ++i)
-      nrf_gpio_cfg_default(NRF_GPIO_PIN_MAP(unused_pins[i].a, unused_pins[i].b));
 }
