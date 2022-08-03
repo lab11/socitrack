f57994a3aa24f38be5d07e1ff84840354dc56ced
Will Hedgecock <will.hedgecock@gmail.com>
2022-08-03 16:04:45 -0500

    Wait until internal RTC has definitively started ticking

--- software/tottag/firmware/peripherals/src/rtc.c
+++ software/tottag/firmware/peripherals/src/rtc.c
@@ -1,5 +1,6 @@
 // Header inclusions ---------------------------------------------------------------------------------------------------
 
+#include "nrf_delay.h"
 #include "nrfx_rtc.h"
 #include "rtc.h"
 
@@ -25,12 +26,21 @@ void rtc_init(void)
    nrfx_rtc_config_t rtc_config = NRFX_RTC_DEFAULT_CONFIG;
    rtc_config.prescaler = 4095;  // Ticks at 8 Hz; PRESCALER is 12bit register (2^12 - 1 = 4095)
    _rtc_sync_time = _rtc_sync_rtc_counter = 0;
-   nrfx_rtc_counter_clear(&_rtc_instance);
    APP_ERROR_CHECK(nrfx_rtc_init(&_rtc_instance, &rtc_config, rtc_handler));
    nrfx_rtc_int_disable(&_rtc_instance, &dummy_val);
    nrfx_rtc_overflow_disable(&_rtc_instance);
    nrfx_rtc_tick_disable(&_rtc_instance);
    nrfx_rtc_enable(&_rtc_instance);
+
+   // Wait until the counter has initiated its first tick and then cleared
+   uint32_t last_tick = nrfx_rtc_counter_get(&_rtc_instance);
+   while (nrfx_rtc_counter_get(&_rtc_instance) == last_tick)
+      nrf_delay_ms(1);
+   while (nrfx_rtc_counter_get(&_rtc_instance) > 8)
+   {
+      nrfx_rtc_counter_clear(&_rtc_instance);
+      nrf_delay_ms(1);
+   }
 }
 
 uint32_t rtc_get_current_time(void)
