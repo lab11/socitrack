650c4ed29b85682e2639db948cda4eb0b834edc7
Will Hedgecock <will.hedgecock@gmail.com>
2023-12-19 14:38:18 -0600

    Fix calculation to locate current page index on startup

--- software/firmware/src/peripherals/src/storage.c
+++ software/firmware/src/peripherals/src/storage.c
@@ -462,7 +462,7 @@ void storage_init(void)
                   uint16_t len = *(uint16_t*)(transfer_buffer+2);
                   if (len < MEMORY_NUM_DATA_BYTES_PER_PAGE)
                   {
-                     current_page = (current_page - MEMORY_PAGES_PER_BLOCK) + i;
+                     current_page += i;
                      cache_index = len;
                      memcpy(cache, transfer_buffer + 4, len);
                      break;
@@ -470,7 +470,7 @@ void storage_init(void)
                }
                else
                {
-                  current_page = (current_page - MEMORY_PAGES_PER_BLOCK) + i;
+                  current_page += i;
                   break;
                }
          }
@@ -657,8 +657,6 @@ uint32_t storage_retrieve_next_data_chunk(uint8_t *buffer)
       else
          memset(buffer, 0xFF, MEMORY_NUM_DATA_BYTES_PER_PAGE);
       reading_page = (reading_page + 1) % BBM_LUT_BASE_ADDRESS;
-      if ((starting_page == current_page) && (reading_page == current_page))
-         is_reading = false;
    }
    return num_bytes_retrieved;
 }
