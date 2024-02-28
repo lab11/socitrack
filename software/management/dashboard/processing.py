d515863fcfc866899d35a981234ba3f6213d3eec
corruptbear <wawnx716@gmail.com>
2024-02-28 15:48:57 -0800

    Allows the use of meter unit in plot; separate data extraction from plotting

--- software/management/dashboard/processing.py
+++ software/management/dashboard/processing.py
@@ -34,18 +34,26 @@ def plot_data(title, x_axis_label, y_axis_label, x_axis_data, y_axis_data):
 def get_voltage_time_series(data, tottag_label):
     voltages = data.loc['v'].dropna()
     timestamps = mdates.date2num([datetime.fromtimestamp(ts) for ts in voltages.keys()])
-    plot_data('Battery Voltage for {}'.format(tottag_label), 'Date and Time', 'Voltage (mV)', timestamps, voltages)
+    plot_data(f"Battery Voltage for {tottag_label}", "Date and Time", "Voltage (mV)", timestamps, voltages)
 
 def get_motion_time_series(data, tottag_label):
     motions = data.loc['m'].ffill()
     timestamps = mdates.date2num([datetime.fromtimestamp(ts) for ts in motions.keys()])
-    plot_data('Motion Status for {}'.format(tottag_label), 'Date and Time', 'Motion Status', timestamps, motions)
+    plot_data(f"Motion Status for {tottag_label}", "Date and Time", "Motion Status", timestamps, motions)
 
-def get_ranging_time_series(data, source_tottag_label, destination_tottag_label, start_timestamp=None, end_timestamp=None, cutoff_distance_ft=30):
-    ranges = data.loc['r.' + destination_tottag_label] / 304.8
-    ranges = ranges.mask(ranges > cutoff_distance_ft)\
+def extract_ranging_time_series(data, destination_tottag_label, start_timestamp=None, end_timestamp=None, cutoff_distance=30, unit="ft"):
+    if unit == "ft":
+        conversion_factor_from_mm = 304.8
+    elif unit == "m":
+        conversion_factor_from_mm = 1000.0
+    ranges = data.loc['r.' + destination_tottag_label] / conversion_factor_from_mm
+    ranges = ranges.mask(ranges > cutoff_distance)\
                    .reindex(pandas.Series(range(start_timestamp if start_timestamp is not None else data.T.head(1).index[0],
                                                 end_timestamp if end_timestamp is not None else 1+data.T.tail(1).index[0])))
     timestamps = mdates.date2num([datetime.fromtimestamp(ts) for ts in ranges.keys()])
-    plot_data('Ranging Data from {} to {}'.format(source_tottag_label, destination_tottag_label),
-              'Date and Time', 'Range (ft)', timestamps, ranges)
+    return timestamps, ranges
+
+def get_ranging_time_series(data, source_tottag_label, destination_tottag_label, start_timestamp=None, end_timestamp=None, cutoff_distance=30, unit="ft"):
+    timestamps, ranges = extract_ranging_time_series(data, destination_tottag_label, start_timestamp=start_timestamp, end_timestamp=end_timestamp, cutoff_distance=cutoff_distance, unit=unit)
+    plot_data(f"Ranging Data from {source_tottag_label} to {destination_tottag_label}",
+              "Date and Time", f"Range ({unit})", timestamps, ranges)
