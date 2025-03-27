412fbf32bcd2c1d231a4aa8b8d40be1bce701992
hapiersiak <123217483+hapiersiak@users.noreply.github.com>
2025-03-26 20:06:29 -0500

    Add files via upload

--- software/management/dashboard/2_convert_to_csv.py
+++ software/management/dashboard/2_convert_to_csv.py
@@ -2,8 +2,8 @@ import csv
 import re
 
 # Input and output file paths
-input_file = "tottagfiles.txt"
-output_file = "tottagfiles.csv"
+input_file = "/Users/hannahpiersiak/Desktop/tottags/ranging_statistics.txt"
+output_file = "/Users/hannahpiersiak/Desktop/tottags/ranging_statistics.txt"
 
 # Regular expressions to extract relevant data
 date_pattern = re.compile(r"Ranging Statistics on (\d{2}/\d{2}/\d{4}):")
